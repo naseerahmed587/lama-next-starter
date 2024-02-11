c2ca9ec4a8299946018b8e06863da0402e7cd0de
naseer <naseerahmed587@gmail.com>
2024-02-11 14:06:40 +0500

    my first commit to test git inline or not

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
+  title: 'My First Next App',
+  description: 'Attempting to learn Next js',
 }
 
 export default function RootLayout({ children }) {
